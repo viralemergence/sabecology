393cec5a05a63fd380ae2dc394d55ad1e6579a69
Timothée <timothee.poisot@umontreal.ca>
2020-04-09 09:33:49 -0400

    Add the ICTV 2018 master file

--- data/scaffold/import_hp3.jl
+++ data/scaffold/import_hp3.jl
@@ -20,13 +20,32 @@ end
 
 entity_match = DataFrame(
     id = UInt64[],
+    type = Symbol[],
     name = String[],
     origin = Symbol[],
     row = Integer[],
     match = Union{UInt64,Nothing}[]
 )
 
-taxonomy = DataFrame(
+host_taxonomy = DataFrame(
+    id = UInt64[],
+    kingdom = Union{String,Missing}[],
+    phylum = Union{String,Missing}[],
+    class = Union{String,Missing}[],
+    order = Union{String,Missing}[],
+    family = Union{String,Missing}[],
+    genus = Union{String,Missing}[],
+    species = Union{String,Missing}[],
+    kingdom_id = Union{Integer,Missing}[],
+    phylum_id = Union{Integer,Missing}[],
+    class_id = Union{Integer,Missing}[],
+    order_id = Union{Integer,Missing}[],
+    family_id = Union{Integer,Missing}[],
+    genus_id = Union{Integer,Missing}[],
+    species_id = Union{Integer,Missing}[]
+)
+
+virus_taxonomy = DataFrame(
     id = UInt64[],
     kingdom = Union{String,Missing}[],
     phylum = Union{String,Missing}[],
