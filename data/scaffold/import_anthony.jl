cfcb4c8afb8421c9365e9ac25e27f85c1ceb955c
Timothée <timothee.poisot@umontreal.ca>
2020-04-17 15:28:22 -0400

    Host parsing for Anthony data

--- data/scaffold/import_anthony.jl
+++ data/scaffold/import_anthony.jl
@@ -15,19 +15,56 @@ anth_host = host_scaffold()
 anth_virus = virus_scaffold()
 anth_associations = associations_scaffold()
 
-## Read the HP3 data
+## Cleanup the ICTV master dataframe
+ictv_master = CSV.read(joinpath(pwd(), "data", "raw", "ictv_master.csv"))[!,1:18]
+select!(ictv_master, Not(Symbol("Type Species?")))
+select!(ictv_master, Not(:Species))
+select!(ictv_master, [:Sort, :Order, :Family, :Genus])
+ictv_records = unique(ictv_master, Not(:Sort))
 
-hp3_virus = CSV.read(joinpath(pwd(), "data", "scaffold", "HP3", "virus_taxonomy.csv"))
-hp3_host = CSV.read(joinpath(pwd(), "data", "scaffold", "HP3", "host_taxonomy.csv"))
-hp3_entities = CSV.read(joinpath(pwd(), "data", "scaffold", "HP3", "entities.csv"))
-hp3_associations = CSV.read(joinpath(pwd(), "data", "scaffold", "HP3", "associations.csv"))
+## Subset the ICTV file
+for virus in eachrow(ictv_records)
+    push!(
+        anth_virus,
+        (hash(virus), virus.Order, virus.Family, virus.Genus)
+    )
+end
+for fam in eachrow(unique(select(ictv_records, [:Order, :Family])))
+    push!(
+        anth_virus,
+        (hash(fam), fam.Order, fam.Family, missing)
+    )
+end
+for ord in unique(anth_virus.viral_order)
+    push!(
+        anth_virus,
+        (hash(ord), ord, missing, missing)
+    )
+end
 
+## Read the Anthony dataset from the CSV file
+anth_data_path = joinpath(pwd(), "data", "raw", "Anthony", "GB_CoV_VRL_noSeqs2.csv")
+anth_raw = CSV.read(anth_data_path; missingstring="NA")
 
-## Read the Anthony dataset from the Excel file
-const data_sheet_name = "GB_CoV_VRL_noSeqs"
-const raw_metadata_path = joinpath("data", "raw", "$(data_sheet_name).xls")
+## Hosts
 
-anth_raw = DataFrame(load(raw_metadata_path, data_sheet_name))
+unique_anth_hosts = filter(!ismissing, unique(anth_raw.gbHost))
 
-## Remove the un-necessary columns
-
+for (i, host) in enumerate(unique_anth_hosts)
+    entity_hash = hash(host*"Anth")
+    match_gbif = missing
+    try
+        match_gbif = taxon(host, strict=false)
+    catch
+        continue
+    end
+    # Add to the taxonomy table
+    match_gbif_hash = ismissing(match_gbif) ? missing : hash(match_gbif)
+    if !isnothing(match_gbif)
+        push!(anth_host,
+            (match_gbif_hash, convert(Tuple, match_gbif)...)
+        )
+    end
+    # Add to the entity table
+    push!(anth_entities, (entity_hash, :host, host, :Anthony, i, match_gbif_hash))
+end
