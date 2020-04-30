0248b1a60c8dbe35590981b3604f88e91f82476b
Timothée <timothee.poisot@umontreal.ca>
2020-04-30 14:14:01 -0400

    kNN results

--- scripts/knn.jl
+++ scripts/knn.jl
@@ -12,41 +12,78 @@ using EcologicalNetworksPlots
 using StatsBase
 
 ## Load the data and aggregate everything
-data_path = joinpath(pwd(), "data", "interactions", "chiroptera.csv")
-chiroptera = CSV.read(data_path)
-select!(chiroptera, Not(:origin))
-chiroptera = unique(chiroptera)
+data_path = joinpath(pwd(), "data", "interactions", "complete.csv")
+virion = CSV.read(data_path)
+select!(virion, Not(:origin))
+select!(virion, Not(:id))
+virion = unique(virion)
 
 ## Prepare the network
-bats = unique(chiroptera.host_species)
-viruses = unique(chiroptera.virus_genus)
-A = zeros(Bool, (length(viruses), length(bats)))
-U = BipartiteNetwork(A, viruses, bats)
+hosts = unique(virion.host_species)
+viruses = unique(virion.virus_genus)
+A = zeros(Bool, (length(viruses), length(hosts)))
+U = BipartiteNetwork(A, viruses, hosts)
 
-for interaction in eachrow(chiroptera)
+for interaction in eachrow(virion)
     U[interaction.virus_genus, interaction.host_species] = true
 end
+
+## Smaller networks
+bats = unique(virion.host_species[findall(virion.host_order .== "Chiroptera")])
+mammals = unique(virion.host_species[findall(virion.host_class .== "Mammalia")])
+
+B = simplify(U[:,bats])
+M = simplify(U[species(B; dims=1),mammals])
 
 ## kNN preparation
 
 tanimoto(x::Set{T}, y::Set{T}) where {T} = length(x∩y)/length(x∪y)
 
 ## Main loop?
-predictions = DataFrame(virus = String[], host = String[], match = Float64[])
-for s in species(U; dims=1)
-    hosts = U[s,:]
-    neighbors = Dict([neighbor => tanimoto(hosts, U[neighbor,:]) for neighbor in filter(x -> x != s, species(U; dims=1))])
-    top_5 = sort(collect(neighbors), by=x->x[2], rev=true)[1:5]
-    hosts_count = StatsBase.countmap(vcat(collect.([U[n.first,:] for n in top_5])...))
-    likely = filter(p -> p.second >= 3, sort(collect(hosts_count), by=x->x[2], rev=true))
-    for l in likely
-        push!(predictions,
-        (s, l.first, l.second/length(top_5))
-        )
+
+function knn_virus(train::T, predict::T; k::Integer=5, cutoff::Integer=1) where {T <: BipartiteNetwork}
+    predictions = DataFrame(virus = String[], host = String[], match = Float64[])
+    for s in species(predict; dims=1)[1:4]
+        @assert s in species(train)
+        hosts = train[s,:]
+        neighbors = Dict([neighbor => tanimoto(hosts, train[neighbor,:]) for neighbor in filter(x -> x != s, species(train; dims=1))])
+        top_k = sort(collect(neighbors), by=x->x[2], rev=true)[1:k]
+        hosts_count = StatsBase.countmap(vcat(collect.([predict[n.first,:] for n in top_k])...))
+        likely = filter(p -> p.second >= cutoff, sort(collect(hosts_count), by=x->x[2], rev=true))
+        for l in likely
+          push!(predictions,
+            (s, l.first, l.second/length(top_k))
+            )
+        end
     end
+    return predictions
 end
 
+
+## Write this shit
+predict_path = joinpath(pwd(), "predictions", "knn")
+ispath(predict_path) || mkpath(predict_path)
+
+## Predict and write
+MtoB = knn_virus(M, B)
+BtoB = knn_virus(B, B)
+
 CSV.write(
-    joinpath(pwd(), "knn.test.csv"),
-    sort(predictions[occursin.("corona", predictions.virus), :], :match, rev=true)
+    joinpath(predict_path, "mammal-bats.all.csv"),
+    MtoB    
+)
+
+CSV.write(
+    joinpath(predict_path, "mammal-bats.corona.csv"),
+    MtoB[occursin.("corona", MtoB.virus),:]
+)
+
+CSV.write(
+    joinpath(predict_path, "bats-bats.all.csv"),
+    BtoB
+)
+
+CSV.write(
+    joinpath(predict_path, "bats-bats.corona.csv"),
+    BtoB[occursin.("corona", BtoB.virus),:]
 )
